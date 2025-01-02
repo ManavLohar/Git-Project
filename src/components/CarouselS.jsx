fcbe9c959995bda5eda71072ef8505becab54b4a
ManavLohar <manavlohar414141@gmail.com>
2025-01-02 13:14:36 +0530

    update App.jsx and add CarouselS.jsx component

---
+++ src/components/CarouselS.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import Carousel from "react-bootstrap/Carousel";
+
+function CarouselS() {
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex) => {
+    setIndex(selectedIndex);
+  };
+
+  return (
+    <Carousel activeIndex={index} onSelect={handleSelect}>
+      <Carousel.Item>
+        <p>Frist Slide</p>
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <p>Second Slide</p>
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <p>Third Slide</p>
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+}
+
+export default CarouselS;
