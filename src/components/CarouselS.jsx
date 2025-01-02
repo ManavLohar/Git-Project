38543e33eb30c0039aaf9164a41ff3ee9e1a45a0
ManavLohar <manavlohar414141@gmail.com>
2025-01-02 17:41:01 +0530

    convert div to p at 24 line

--- src/components/CarouselS.jsx
+++ src/components/CarouselS.jsx
@@ -21,16 +21,14 @@ function CarouselS() {
         <p>Second Slide</p>
         <Carousel.Caption>
           <h3>Second slide label</h3>
-          <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</div>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <p>Third Slide</p>
         <Carousel.Caption>
           <h3>Third slide label</h3>
-          <h3>Fifth slide label</h3>
           <h3>Fourth slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
           <p>
             Praesent commodo cursus magna, vel scelerisque nisl consectetur.
           </p>
