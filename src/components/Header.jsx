317b9ec84f3c736289b4117d46f3121593bc24c2
ManavLohar <manavlohar414141@gmail.com>
2025-01-02 13:53:56 +0530

    Update Header file with some changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -17,7 +17,7 @@ const Header = () => {
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
-                Another action
+                This is Another And Next Level Action
               </NavDropdown.Item>
               <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
               <NavDropdown.Divider />
